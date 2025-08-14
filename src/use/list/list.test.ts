4e4f6006e7a901306a284b6001be04e6eed79cd2
xuyimingwork <xuyimingwork@foxmail.com>
2025-08-14 15:40:15 +0800

    refactor: useList

--- src/use/list/list.test.ts
+++ src/use/list/list.test.ts
@@ -1,23 +1,181 @@
 import { useList } from "@/use/list/list";
-import { expect, test } from 'vitest'
+import { expect, test, vi } from 'vitest'
 
-test('allow query return undefined', () => {
+const wait = (ms = 0) => new Promise((resolve) => setTimeout(resolve, ms))
+const createSource = () => ({
+  query: ({ pageIndex, pageSize }: any) => ({
+    total: 100,
+    data: new Array((100 - (pageIndex * pageSize)) < 0
+      ? 0
+      : (100 - (pageIndex * pageSize)) < pageSize
+        ? (100 - (pageIndex * pageSize))
+        : pageSize
+    ).fill(0).map((v, i) => pageIndex * pageSize + i)
+  })
+})
+
+test('before first query state', async () => {
+  const { data } = useList({ query: () => ([1]) as any })
+  expect(data.value).toBeInstanceOf(Array)
+  expect(data.value.length).toBe(0)
+})
+
+test('after first query state', async () => {
+  const { data,  reload } = useList({ query: () => ([1]) as any })
+  reload()
+  await wait()
+  expect(data.value).toBeInstanceOf(Array)
+  expect(data.value.length).toBe(1)
+})
+
+test('allow query return undefined', async () => {
   const { data, reload } = useList({ query: () => (undefined) as any })
+  const v = data.value
   reload()
-  expect(data.value).toBeInstanceOf(Array)
-  expect(data.value.length).toBe(0)
+  await wait()
+  expect(data.value).toBe(v)
 })
 
-test('allow query return async undefined', () => {
-  const { data: data, reload: reload } = useList({ query: () => Promise.resolve() as any })
+test('allow query return async undefined', async () => {
+  const { data, reload } = useList({ query: () => Promise.resolve() as any })
+  const v = data.value
   reload()
-  expect(data.value).toBeInstanceOf(Array)
-  expect(data.value.length).toBe(0)
+  await wait()
+  expect(data.value).toBe(v)
 })
 
-test('allow query return addition value', () => {
+test('allow query return addition value', async () => {
   const { response, reload } = useList({ query: () => ({ total: 3, data: [1, 2, 3], abc: 'ok' }) })
   expect(response.value).toBe(undefined)
   reload()
+  await wait()
   expect(response.value.abc).toBe('ok')
 })
+
+test('reload immediate update page', async () => {
+  const source = createSource()
+  const { reload, pageIndex } = useList({ query: (options: any) => source.query(options) })
+  pageIndex.value++
+  expect(pageIndex.value).toBe(1)
+  reload()
+  expect(pageIndex.value).toBe(0)
+})
+
+test('query called on same delay', async () => {
+  const source = createSource()
+  const spyQuery = vi.spyOn(source, 'query')
+  const {
+    pageIndex,
+    pageSize,
+    reload,
+    refresh,
+  } = useList({ query: (options: any) => source.query(options) })
+  pageSize.value = 20
+  await wait(0)
+  expect(spyQuery).toBeCalledTimes(1)
+
+  pageIndex.value++
+  await wait(0)
+  expect(spyQuery).toBeCalledTimes(2)
+
+  reload()
+  await wait(0)
+  expect(spyQuery).toBeCalledTimes(3)
+
+  refresh()
+  await wait(0)
+  expect(spyQuery).toBeCalledTimes(4)
+})
+
+test('change page size will auto change index', async () => {
+  const source = createSource()
+  const {
+    pageSize,
+    pageIndex,
+  } = useList({ query: (options: any) => {
+    return source.query(options)
+  }})
+
+  pageIndex.value++
+  expect(pageIndex.value).toBe(1)
+  pageSize.value = 2
+  expect(pageIndex.value).toBe(0)
+})
+
+test('change page size will auto query', async () => {
+  const source = createSource()
+  const spyQuery = vi.spyOn(source, 'query')
+  const {
+    pageSize,
+  } = useList({ query: (options: any) => {
+    return source.query(options)
+  }})
+
+  pageSize.value = 2
+  await wait(0)
+  expect(spyQuery).toBeCalledTimes(1)
+})
+
+test('change page index will auto query', async () => {
+  const source = createSource()
+  const spyQuery = vi.spyOn(source, 'query')
+  const {
+    pageIndex,
+  } = useList({ query: (options: any) => {
+    return source.query(options)
+  }})
+
+  pageIndex.value++
+  await wait(0)
+  expect(spyQuery).toBeCalledTimes(1)
+})
+
+test('change page variable both should query only once', async () => {
+  const source = createSource()
+  const spyQuery = vi.spyOn(source, 'query')
+  const {
+    pageIndex,
+    pageSize,
+  } = useList({ query: (options: any) => {
+    return source.query(options)
+  }})
+
+  pageIndex.value = 2
+  pageSize.value = 2
+  await wait(0)
+  expect(spyQuery).toBeCalledTimes(1)
+})
+
+test('change page variable order: index first', async () => {
+  const source = createSource()
+  const {
+    pageIndex,
+    pageSize,
+  } = useList({ query: (options: any) => {
+    expect(options.pageIndex).toBe(0)
+    expect(options.pageSize).toBe(2)
+    return source.query(options)
+  }})
+
+  pageIndex.value = 2
+  pageSize.value = 2
+  await wait(0)
+})
+
+test('change page variable order: size first', async () => {
+  const source = createSource()
+  const {
+    pageIndex,
+    pageSize,
+  } = useList({ query: (options: any) => {
+    expect(options.pageIndex).toBe(2)
+    expect(options.pageSize).toBe(2)
+    return source.query(options)
+  }})
+
+  pageSize.value = 2
+  pageIndex.value = 2
+  await wait(0)
+})
+
+
--- src/use/list/list.ts
+++ src/use/list/list.ts
@@ -1,9 +1,11 @@
-// list = [query params] + table + [pagination]
-// pagination => index + size + total
-
 import { useAsyncData } from "vue-asyncx"
 import type { Ref, ComputedRef } from 'vue'
+import { assign, debounce, isInteger, isObjectLike, isUndefined, omitBy } from "es-toolkit/compat"
 
+/**
+ * 对参数的修改发生于同个 tick
+ * 请求发起于下一个 timeout
+ */
 type MaybePromise<T = any> = T | Promise<T> | PromiseLike<T>
 type Query<P, I> = (options: { params: P, pageIndex: number, pageSize: number }) => MaybePromise<Array<I> | { data: Array<I>, total: number }>
 type Data<Q extends Query<any, any>> = (Awaited<ReturnType<Q>>) extends Array<infer D>
@@ -11,6 +13,9 @@ type Data<Q extends Query<any, any>> = (Awaited<ReturnType<Q>>) extends Array<in
   : (Awaited<ReturnType<Q>>) extends ({ data: Array<infer D> })
     ? Array<D>
     : [] // query 返回异常参数时，类型为空数组
+
+const isValidPageIndex = (v: any): v is number => isInteger(v) && v > -1
+const isValidPageSize = (v: any): v is number => isInteger(v) && v > 0
 
 export function useList<
   Q extends Query<any, any> = any
@@ -33,25 +38,39 @@ export function useList<
   refresh: () => void, // 操作
 } {
   const params = ref<any>({})
-  const pageIndex = ref<number>(0)
-  const pageSize = ref(25)
+  const _pageIndex = ref<number>(0)
+  const _pageSize = ref(25)
 
   const {
     data: rawData,
     queryDataLoading: loading,
-    queryData: queryRawData
+    queryData: _queryRawData
   } = useAsyncData(() => {
     return query({
       params: params.value,
-      pageIndex: pageIndex.value,
-      pageSize: pageSize.value
+      pageIndex: _pageIndex.value,
+      pageSize: _pageSize.value
     })
   }, {
     initialData: undefined
   })
 
-  // 初始状态总项数为 0
-  // 单页状态总项数为列表项数
+  const queryRawData = debounce(_queryRawData)
+
+  function _query(options?: {
+    params?: Partial<Parameters<Q>['0']['params']>
+    pageIndex?: number,
+    pageSize?: number
+  }): void {
+    if (!options) return queryRawData(), undefined
+
+    if (isValidPageIndex(options.pageIndex)) _pageIndex.value = options.pageIndex
+    if (isValidPageSize(options.pageSize)) _pageSize.value = options.pageSize
+    if (isObjectLike(options.params) && Object.keys(options.params as object).length) params.value = omitBy(assign({}, params.value, options.params), isUndefined)
+
+    queryRawData()
+  }
+
   const result = computed<{ data: Data<Q>, total: number }>(() => {
     if (Array.isArray(rawData.value)) return { data: rawData.value as any, total: rawData.value.length }
 
@@ -60,27 +79,34 @@ export function useList<
 
     return { data: [], total: 0 }
   })
-
   const data = computed(() => result.value.data)
   const total = computed(() => result.value.total)
 
-  // 第 1 页，25 项/页，第 1 项为 0
-  // 第 2 页，25 项/页，第 1 项为 25
+  const pageIndex = computed({
+    get: () => _pageIndex.value,
+    set: v => {
+      if (!isValidPageIndex(v) || _pageIndex.value === v) return
+      _pageIndex.value = v
+      refresh()
+    }
+  })
+  const pageSize = computed({
+    get: () => _pageSize.value,
+    set: v => {
+      if (!isValidPageSize(v) || _pageSize.value === v) return
+      _pageSize.value = v
+      reload()
+    }
+  })
   const rowIndex = computed(() => pageIndex.value * pageSize.value)
 
   function reload() {
-    pageIndex.value = 0
-    queryRawData()
+    _query({ pageIndex: 0 })
   }
 
   function refresh() {
-    queryRawData()
+    _query()
   }
-
-  // 配置化触发
-  watch(pageIndex, () => refresh())
-  // 配置化触发
-  watch(pageSize, () => reload())
 
   return {
     params, // 传参（读写）
@@ -93,7 +119,7 @@ export function useList<
     total, // 结果（只读）
     rowIndex, // 结果（只读）
 
-    reload, // 操作
-    refresh, // 操作
+    refresh, // 刷新当前页
+    reload, // 重首页重载数据
   }
 }
