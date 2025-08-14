c220fac6d011f057393397a01a2c0daecc4055fe
xuyimingwork <xuyimingwork@foxmail.com>
2025-08-14 15:36:33 +0800

    fix: add comment

--- src/flex/FlexIndex.vue
+++ src/flex/FlexIndex.vue
@@ -62,32 +62,32 @@
     box-sizing: border-box;
 
     & > &__main {
-      flex: auto; /* 1 1 auto */
+      flex: auto; /* 1 1 auto 自动伸缩 */
     }
     & > &__start,
     & > &__end {
-      flex: none; /* 0 0 auto */
+      flex: none; /* 0 0 auto 不允许伸缩 */
     }
 
     &--row,
     &--row-reverse {
-      width: 100%;
-      overflow-x: auto;
+      width: 100%; // 默认行为，保持代码结构一致性
+      overflow-x: auto; // start end 超出水平空间，由于 start、end 无法压缩，故允许容器水平滚动
 
       & > #{$self}__main {
-        min-width: 0;
-        overflow-x: auto;
+        min-width: 0; // 默认情况下，允许 main 部分被压缩至消失
+        overflow-x: auto; // good: 超出主内容区域，允许滚动 bad: 内容 100% 情形下会导致 box-shadow 被 hidden
       }
     }
 
     &--column,
     &--column-reverse {
       height: 100%; // 垂直状态默认占据垂直向 100% 空间，与水平向保持一致
-      overflow-y: auto;
+      overflow-y: auto; // start、end 超出垂直空间，由于 start、end 无法压缩，故允许容器垂直滚动
 
       & > #{$self}__main {
-        min-height: 0;
-        overflow-y: auto;
+        min-height: 0; // 默认情况下，允许 main 部分被压缩至消失
+        overflow-y: auto;  // good: 超出主内容区域，允许滚动 bad: 内容 100% 情形下会导致 box-shadow 被 hidden
       }
     }
 
